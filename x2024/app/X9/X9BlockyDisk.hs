fa45cb81c96018d16d58c240f1d7c302a926b6c9
Scharage <scharage3@gmail.com>
2024-12-11 20:35:07 +0100

    Day 9 Part 2

--- x2024/app/X9/X9BlockyDisk.hs
+++ x2024/app/X9/X9BlockyDisk.hs
@@ -39,10 +39,10 @@ splitFree = Seq.spanr isFree
 
 splitLastBlock :: FastDisk -> (FastDisk, FastDisk)
 splitLastBlock d = spl d
-                where spl Seq.Empty = (Seq.empty,d)  
+                where spl Seq.Empty = (Seq.empty,d)
                       spl (Seq.viewr -> body Seq.:> e) = (Seq.singleton e,body)
-                      spl _ = (Seq.empty,d)  
-                       
+                      spl _ = (Seq.empty,d)
+
 
 takenToFree::Space->Space
 takenToFree (Taken _ n) = Free n
@@ -53,23 +53,54 @@ diskTakenToFree d = spl d
                 where spl (Seq.viewl -> h Seq.:< _) = takenToFree h
                       spl _ = Free 0
 
+fitsInto :: Space -> Space -> Bool
+fitsInto (Free x) (Free y) = x >= y
+fitsInto (Free x) (Taken _ y) = x >= y
+fitsInto _ _ = False
+
+takeSingleton::FastDisk -> Space
+takeSingleton (Seq.viewl -> h Seq.:< _) = h
+takeSingleton _ = Free 0
+
+takeHead::FastDisk -> (FastDisk,FastDisk)
+takeHead (Seq.viewl -> h Seq.:< b) = (Seq.singleton h,b)
+takeHead b = (Seq.empty,b)
+
 splitNextFreeBlock:: FastDisk -> FastDisk -> (FastDisk,FastDisk,FastDisk)
 splitNextFreeBlock blockToFit spaceToLook = (freeSpace,before,after)
                                             where matchingFree = diskTakenToFree blockToFit
-                                                  (before,rest) = Seq.breakl (==matchingFree) spaceToLook
-                                                  (freeSpace,after) = Seq.spanr (==matchingFree) rest
+                                                  (before,rest) = Seq.breakl (\x -> fitsInto x (takeSingleton blockToFit)) spaceToLook
+                                                  (freeSpace,after) = takeHead rest
 
+diffSpace::FastDisk -> FastDisk -> FastDisk
+diffSpace (Seq.viewl -> (Free x) Seq.:< _) (Seq.viewl -> (Taken _ y) Seq.:< _)| diff <= 0 = Seq.empty
+                                                                              | otherwise = Seq.singleton (Free diff)
+                                                                              where diff = x-y
+diffSpace _ _ = Seq.empty
 
 sortDisk':: FastDisk -> FastDisk
 sortDisk' d | Seq.null d = Seq.empty
             | lastBlock == Seq.empty = Seq.empty
-            | freeBlock == Seq.singleton (Free 0) = sortDisk' r2  Seq.>< lastBlock Seq.>< freeD
-            | otherwise = sortDisk' (before Seq.>< lastBlock Seq.>< after) Seq.>< freeBlock Seq.>< freeD
-            where 
+            | freeBlock == Seq.empty = sortDisk' r2  Seq.>< lastBlock Seq.>< freeD
+            | otherwise = sortDisk' (before Seq.>< lastBlock Seq.>< spaceLeft Seq.>< after) Seq.>< spaceTaken Seq.>< freeD
+            where
                   (freeD,r) = splitFree d
                   (lastBlock,r2) = splitLastBlock r
                   (freeBlock,before,after) = splitNextFreeBlock lastBlock r2
+                  spaceLeft = diffSpace freeBlock lastBlock
+                  spaceTaken = Seq.singleton (diskTakenToFree lastBlock)
 
 sortDisk::Disk -> Disk
 sortDisk [] = []
 sortDisk d = Foldr.toList $ sortDisk' $ Seq.fromList d
+
+mulSpaceIndex::(Space,Int)->Int
+mulSpaceIndex (Free _,_) = 0
+mulSpaceIndex (Taken did _,i) = i * did
+
+breakIntoSingleSpace::Space -> FastDisk
+breakIntoSingleSpace (Free n) = Seq.fromList$ replicate n (Free 1)
+breakIntoSingleSpace (Taken i n) = Seq.fromList$ replicate n (Taken i 1)
+
+checksum::FastDisk -> Int
+checksum d = sum (mulSpaceIndex <$> Seq.mapWithIndex (\i x-> (x,i)) (d >>= breakIntoSingleSpace))
--- x2024/app/X9/X9Main.hs
+++ x2024/app/X9/X9Main.hs
@@ -2,16 +2,17 @@
 module X9.X9Main where
 import X9.X9Disk
 import qualified X9.X9BlockyDisk as B'
+import qualified Data.Sequence as Seq
 
 
 x9main :: IO ()
 x9main = do
-    input <- readFile "./app/X9/input_debug.txt"
+    input <- readFile "./app/X9/input.txt"
     let disk = diskMapToDisk input
     let sortedDisk = sortDisk disk
     let sortedChecksum = checksum sortedDisk
     let b'disk = B'.diskMapToDisk input
     let b'sortedDisk = B'.sortDisk b'disk
-    let b'sortedChecksum = checksum sortedDisk
+    let b'sortedChecksum = B'.checksum $ Seq.fromList b'sortedDisk
     print $ "Checksum of the Disk: " ++ show sortedChecksum
-    print $ "Checksum of Blocky Disk: " ++ show b'sortedDisk
+    print $ "Checksum of Blocky Disk: " ++ show b'sortedChecksum
